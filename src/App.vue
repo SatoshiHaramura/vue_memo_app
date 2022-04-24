145dd686176c6e7d5a5fb9d0a900a468e2b6f971
SatoshiHaramura <satoshi.haramura@gmail.com>
2022-04-24 22:05:33 +0900

    add: Add new files

---
+++ src/App.vue
@@ -0,0 +1,103 @@
+<template>
+  <div id="app">
+    <ListMemo
+    :memos="memos"
+    @change-edit-mode="changeEditMode"
+    @change-append-mode="changeAppendMode"/>
+
+    <EditMemo
+    v-show="isEditMode || isAppendMode"
+    :memo="memos[targetIndex]"
+    @update-memo="updateMemo"
+    @delete-memo="deleteMemo"/>
+  </div>
+</template>
+
+<script>
+import ListMemo from '@/components/ListMemo'
+import EditMemo from '@/components/EditMemo'
+const LOCAL_STORAGE_KEY = 'memos'
+const LIST_MODE = 'LIST_MODE'
+const EDIT_MODE = 'EDIT_MODE'
+const APPEND_MODE = 'APPEND_MODE'
+
+export default {
+  name: 'App',
+  components: {
+    ListMemo,
+    EditMemo
+  },
+  data() {
+    return {
+      memos: [],
+      currentMode: LIST_MODE,
+      targetIndex: undefined
+    }
+  },
+  mounted() {
+    if (localStorage.getItem(LOCAL_STORAGE_KEY)) {
+      try {
+        this.memos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+      } catch(e) {
+        localStorage.removeItem(LOCAL_STORAGE_KEY)
+      }
+    }
+  },
+  computed: {
+    isEditMode() {
+      return this.currentMode === EDIT_MODE
+    },
+    isAppendMode() {
+      return this.currentMode === APPEND_MODE
+    }
+  },
+  methods: {
+    changeEditMode(index) {
+      this.currentMode = EDIT_MODE
+      this.targetIndex = index
+    },
+    changeAppendMode() {
+      this.currentMode = APPEND_MODE
+      this.targetIndex = undefined
+    },
+    changeListMode() {
+      this.currentMode = LIST_MODE
+      this.targetIndex = undefined
+    },
+    updateMemo(memo) {
+      if (!memo) return
+      if (this.isEditMode) {
+        this.memos.splice(this.targetIndex, 1, memo)
+      } else if (this.isAppendMode) {
+        this.memos.push(memo)
+      }
+      this.saveMemos()
+      this.changeListMode()
+    },
+    deleteMemo() {
+      if (this.isEditMode) {
+        this.memos.splice(this.targetIndex, 1)
+        this.saveMemos()
+      }
+      this.changeListMode()
+    },
+    saveMemos() {
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(this.memos))
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import 'bulma/css/bulma.css';
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+
+  margin: 100px 100px;
+  border: 1px solid #ccc;
+  border-radius: 10px 10px 10px 10px
+}
+</style>
---
+++ src/components/EditMemo.vue
@@ -0,0 +1,60 @@
+<template>
+  <section class="section">
+    <div class="content">
+      <h1 class="title">編集</h1>
+      <textarea v-model.trim="inputItem" class="textarea" placeholder="メモを入力してください。" rows="5">
+      </textarea><br>
+      <div class="field is-grouped">
+        <p class="control">
+          <button @click="updateMemo" type="button" class="button is-success is-outlined is-rounded">編集
+          </button>
+        </p>
+        <p class="control">
+          <button @click="deleteMemo" type="button" class="button is-danger is-outlined is-rounded">削除
+          </button>
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'EditMemo',
+  props: {
+    memo: String
+  },
+  data() {
+    return {
+      enteredItem: ' '
+    }
+  },
+  computed: {
+    inputItem: {
+      get() {
+        return this.memo
+      },
+      set(memo) {
+        this.enteredItem = memo
+      }
+    }
+  },
+  methods: {
+    updateMemo() {
+      this.$emit('update-memo', this.enteredItem)
+      this.initializeEnterItem()
+    },
+    deleteMemo() {
+      this.$emit('delete-memo')
+      this.initializeEnterItem()
+    },
+    initializeEnterItem() {
+      this.enteredItem = ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
